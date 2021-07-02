73af07a8707c6ce8d3b96dfc9fe22dce8ca92cfc
Eli Pacheco <elipacheco@elis-air.lan>
2021-07-02 17:05:03 -0600

    have fun

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # balance_your_life
 A program to help balance your checkbook
+
+I call the program "Balance your life", to help balance your checking and bills
+
+Simply add the amount you have to work with (usually checking), and the program will walk you through the rest
---
+++ main_code.py
@@ -0,0 +1,34 @@
+
+# color options
+class bcolors:
+    HEADER    = '\033[95m'
+    OKBLUE    = '\033[94m'
+    OKGREEN   = '\033[92m'
+    WARNING   = '\033[93m'
+    FAIL      = '\033[91m'
+    BOLD      = '\033[1m'
+    UNDERLINE = '\033[4m'
+    ENDC      = '\033[0m'
+
+ticker = 0
+counter = '1'
+accounts_and_balances = {}
+
+balance_in_checking = float(input("How much money do we have to work with? (Usually the money in your checking)\nAnswer in dollars and cents: "))
+accounts_to_pay = int(input("How many accounts do you have to pay today? "))
+num_of_accounts = accounts_to_pay*counter
+
+for i in range(len(num_of_accounts)):
+    ticker += 1
+    names = str(input(f'Please name account {ticker} '))
+    balances = float(input(f"Please enter the balance of {names}, in dollars and cents "))
+    accounts_and_balances[names] = balances
+
+for i,j in accounts_and_balances.items():
+    payment = float(input(f"You owe {j} on account {i}. How much do you want to pay?\nEnter your answer in dollars and cents: "))
+    new_balance = balance_in_checking - payment
+    account_balance = j - payment
+    print(bcolors.OKBLUE + f'Your new checking balance is: {new_balance}\nYour balance on account {i} is: {account_balance}' + bcolors.ENDC)
+    print("And we Continue...")
+
+print("Thank you for using Eli Pachecos " + bcolors.HEADER + "BALANCE YOUR LIFE" + bcolors.ENDC + " program\nPlease use again next month!")
