8cf0b506e01d03a592a554c6f83c1e62950eb676
Eli Pacheco <elipacheco@elis-air.lan>
2021-07-16 12:07:24 -0600

    comments

--- main_code.py
+++ main_code.py
@@ -3,6 +3,7 @@
 from src.functions import questions, dictionary_work
 from src.colors import bcolors
 
+# calling functions from src
 questions()
 dictionary_work()
-print("Thank you for using Eli Pachecos " + bcolors.HEADER + "BALANCE YOUR LIFE" + bcolors.ENDC + " program\nPlease use again next month!")
+print("Thank you for using Eli Pacheco's " + bcolors.HEADER + "BALANCE YOUR LIFE" + bcolors.ENDC + " program\nPlease use again next month!")
--- src/functions.py
+++ src/functions.py
@@ -2,9 +2,12 @@
 from src.colors import bcolors
 from src.calculator import Calculator
 
+# initiate the global variables as floats and integers
 balance_in_checking = float
-accounts_to_pay = int
+accounts_to_pay = int 
+# initiate a length counter for the amount of accounts
 length_counter = '1'
+# initiate a counter for account(s) 1 - n
 ticker = 0
 
 def questions():
@@ -22,11 +25,14 @@ def questions():
         print("Please run the program again.")
 
 def dictionary_work():
+    # calling the global variables
     global balance_in_checking
     global accounts_to_pay
     global length_counter
     global ticker
+    # initiate the dictionary
     accounts_and_balances = {}
+    # use length counter to create a length for the loop (1*n)
     num_of_accounts = accounts_to_pay*length_counter
 
     for i in range(len(num_of_accounts)):
