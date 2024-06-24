f12c52c19fd90902834c34a5666dee3596c7d683
Eli Pacheco <77797720+elip55@users.noreply.github.com>
2024-06-24 12:20:15 -0600

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -30,13 +30,14 @@ class BalanceLife:
 def payments(checking,list1,list2):
     payments = []
     account_payment_balance = {}
-
+    checking_balance = checking
     for i in range(len(list1)-1):
         print(f'For account:' + bcolors.BOLD + f'{list1[i+1]}\n' + bcolors.ENDC + 'You have a balance of: ' + bcolors.BOLD + f'{list2[0][i+1]}\n' + bcolors.ENDC + 'And a minimum payment of: ' + bcolors.FAIL + f'{list2[1][i+1]}.' + bcolors.ENDC)
         ticker = 0
         while True:
             try:
                 payment = float(input('How much would you like to pay today? '))
+                checking_balance = checking_balance - round(payment, 2)
                 break
             except:
                 print(bcolors.FAIL + 'YOU HAVE FAILED A SIMPLE TASK, PAY ATTENTION AND INPUT DOLLARS AND CENTS!!' + bcolors.ENDC)
@@ -45,12 +46,13 @@ def payments(checking,list1,list2):
                     print(bcolors.FAIL + 'YOU MUST BE CONFUSED, PLEASE RUN THIS PROGRAM AGAIN!' + bcolors.ENDC)
                     exit()
         new_balance = list2[0][i+1] - payment
-
+        checking_balance = round(checking_balance,2)
+        print(bcolors.BOLD + f'\nYour checking balance is now: {checking_balance}\n' + bcolors.ENDC)
         payments.append(payment)
         account_payment_balance[list1[i+1]] = [payment, round(new_balance, 2)]
     
     u = round(sum(payments), 2)
-    checking = checking - u
+    checking = round((checking - u),2)
     f = BalanceLife(account_payment_balance, checking)
     f.excel_is_for_nerds()
 
